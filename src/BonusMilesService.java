69c5d02b7aba00dbea6d64121a7f7715fdcc1645
Ivan Borisov <ivan_borisov_88@vk.com>
2023-01-10 19:24:43 +0400

    change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
+    public int calculate(int cost) {
         int mile = 20;
-        int bonus = cost/mile;
+        int bonus = cost / mile;
         return bonus;
     }
 }
